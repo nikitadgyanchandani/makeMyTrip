6a8c0e3ba37f648b08742469658e0479328c2a4d
ZSPL\nikita.gyanchandani <nikitadgyanchandani@gmail.com>
2021-09-23 20:07:26 +0530

    Commit updated code

--- src/main/java/com/makemytrip/PageObjects/HomePage.java
+++ src/main/java/com/makemytrip/PageObjects/HomePage.java
@@ -1,10 +1,13 @@
 package com.makemytrip.PageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class HomePage {
 	
@@ -12,15 +15,44 @@ public class HomePage {
 	
 
 	By Logo=By.xpath("//a[contains(@class,'mmtLogo makeFlex')]");
+	By langCurrencyDropdown=By.xpath("//span[contains(@class,'switcherDownArrow')]");
+	By searchButton=By.xpath("//a[text()='Search']");
+	By country=By.xpath("//div[@class='searchListWrap']/p");
+	
+	
+	public HomePage(WebDriver driver)
+	{
+		this.driver=driver;
+	}
 	
 	public WebElement verifyLogo()
 	{
 		return driver.findElement(Logo);
 		
 	}
-	
-	public HomePage(WebDriver driver)
+	public WebElement verifyLangCurrencyCountry()
 	{
-		this.driver=driver;
+		return driver.findElement(langCurrencyDropdown);
+		
 	}
-}
+	/*public void verifyCountryList()
+	{
+		Select dropdown = new Select(country);
+
+	    //Get all options
+	    List<WebElement> dd = dropdown.getOptions();
+	    for (int j = 0; j < dd.size(); j++) 
+	    {
+	        System.out.println(dd.get(j).getText());
+	    }
+		//return dd;
+*/	
+	
+	public WebElement serachButton()
+	{
+		return driver.findElement(searchButton);
+	}
+	
+	}
+	
+
--- src/test/java/com/makemytrip/TestCases/HomePageTest.java
+++ src/test/java/com/makemytrip/TestCases/HomePageTest.java
@@ -1,7 +1,13 @@
 package com.makemytrip.TestCases;
 
 import java.io.IOException;
+import java.util.List;
 
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import com.makemytrip.CommonUtilities.baseClass;
@@ -9,21 +15,55 @@ import com.makemytrip.PageObjects.HomePage;
 
 public class HomePageTest extends baseClass{
 	
-	@Test
+	HomePage hp=new HomePage(driver);
+	
+	@BeforeMethod
 	public void baseClassNavigation() throws IOException
 	{
 		driver=initializeDriver();
 		driver.get("https://www.makemytrip.com/");
 		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		
-		HomePage hp=new HomePage(driver);
+		driver.manage().deleteAllCookies();			
+	}
+	
+	@Test(priority=1)
+	public void verifyLogo()
+	{
 		boolean logoIsDisplayed=hp.verifyLogo().isDisplayed();
 		
-		System.out.println("baseClassNavigation: ,Expected Value: true, Actual value: " + logoIsDisplayed);
-		System.out.println("git commit successfully done");
-		System.out.println("git commit successfully done");
-				
+		System.out.println("verifyLogo: ,Expected Value: true, Actual value: " + logoIsDisplayed);
+		
+	}
+	
+	@Test(priority=2)
+	public void verifyLangCurrencyCountryDropdown()
+	{
+		
+		boolean dropdownIsDisplayed=hp.verifyLangCurrencyCountry().isDisplayed();
+		System.out.println(dropdownIsDisplayed);
+		hp.verifyLangCurrencyCountry().click();
+		
+	}
+	
+	/*@Test
+	public void verifyCountryList()
+	{
+		
+		hp.verifyCountryList();
+	}*/
+	
+	@Test(priority=3)
+	public void  verifySearchButton()
+	{
+
+		boolean buttonIsDisplayed=hp.serachButton().isDisplayed();
+		System.out.println("verifySearchButton: ,Expected Value: true, Actual value: " + buttonIsDisplayed);
+	}
+	
+	@AfterMethod
+	public void tearDown()
+	{
+		driver.close();
 	}
 
 }
